025307ecee5501ff5cc888dff60cc3b486f111b7
leechangjun <toprank@tricycle.co.kr>
2020-07-29 14:20:36 +0900

    Initial commit !

---
+++ src/test/java/com/tricycle/gate/GateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tricycle.gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
