3a1755695d542da6e68c949540cd838b6800d005
leechangjun <toprank@tricycle.co.kr>
2020-08-10 14:17:33 +0900

    setting

--- src/main/java/com/tricycle/gate/GateApplication.java
+++ src/main/java/com/tricycle/gate/GateApplication.java
@@ -2,9 +2,10 @@ package com.tricycle.gate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GateApplication {
+public class GateApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GateApplication.class, args);
