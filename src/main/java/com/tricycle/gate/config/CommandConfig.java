0105d545a1d5c6e34664e0e1683694f86ff86622
leechangjun <toprank@tricycle.co.kr>
2020-08-13 16:44:19 +0900

    test

---
+++ src/main/java/com/tricycle/gate/config/CommandConfig.java
@@ -0,0 +1,21 @@
+package com.tricycle.gate.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class CommandConfig implements CommandLineRunner {
+  @Value("${spring.profiles.active}")
+  private String profiles;
+
+  @Override
+  public void run(String... args) throws Exception {
+    log.debug("{}", profiles);
+  }
+}
