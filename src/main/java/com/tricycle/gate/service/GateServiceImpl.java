4dd147086f4a5131ce5be8c8e5e2ee539e3cf3b4
김지연 <jiyeon@tricycle.co.kr>
2020-12-07 15:01:17 +0900

    partner_id log 추가

--- src/main/java/com/tricycle/gate/service/GateServiceImpl.java
+++ src/main/java/com/tricycle/gate/service/GateServiceImpl.java
@@ -166,6 +166,7 @@ public class GateServiceImpl implements GateService {
 			// 에러 Log Insert
 			excptLogMap.put("log_seq", partnerConnSeq);
 			excptLogMap.put("log_type","1");
+			excptLogMap.put("partner_id", partnerId);
 			int res = mysqlGateMapper.insertExcptLog(excptLogMap);
 			// 기본 매출코드 셋팅 후 홈 랜딩
 
@@ -254,6 +255,7 @@ public class GateServiceImpl implements GateService {
 				// 에러 Log Insert
 				excptLogMap.put("log_seq", partnerConnSeq);
 				excptLogMap.put("log_type","2");
+				excptLogMap.put("partner_id", partnerId);
 				int res = mysqlGateMapper.insertExcptLog(excptLogMap);
 				// 기본 매출코드 셋팅 후 홈 랜딩
 
@@ -467,6 +469,7 @@ public class GateServiceImpl implements GateService {
 			// 에러 Log Insert
 			excptLogMap.put("log_seq", partnerConnSeq);
 			excptLogMap.put("log_type","1");
+			excptLogMap.put("partner_id", partnerId);
 			int res = mysqlGateMapper.insertExcptLog(excptLogMap);
 
 			// response에 쿠키로 mnm 에 사이트별 기본 매출코드 심기
@@ -545,6 +548,7 @@ public class GateServiceImpl implements GateService {
 				// 에러 Log Insert
 				excptLogMap.put("log_seq", partnerConnSeq);
 				excptLogMap.put("log_type","2");
+				excptLogMap.put("partner_id", partnerId);
 				int res = mysqlGateMapper.insertExcptLog(excptLogMap);
 
 				// response에 쿠키로 mnm 에 사이트별 매출코드 심기
@@ -739,6 +743,7 @@ public class GateServiceImpl implements GateService {
 		cookie.setHttpOnly(false);
 		response.addCookie(cookie);
 
+		setCookie(response, siteCd, "_partid_", this.partnerId, 60 * 60 * 6, false, false);
 		setCookie(response, siteCd, "NFG", "Y", 60 * 60 * 6, false, false);
 	}
 
